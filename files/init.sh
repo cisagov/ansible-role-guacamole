737a96d3d34f5bd22767c37dc50e52b803c0bc89
Kyle Evers <kyleevers14@yahoo.com>
2019-06-10 14:40:24 -0400

    move password to stored secure string ssm variable

--- files/init.sh
+++ files/init.sh
@@ -2,6 +2,6 @@
 
 createdb guacamole_db
 cat /var/log/docker/initdb.sql | psql -d guacamole_db -f -
-psql -c "CREATE USER guacamole_user WITH PASSWORD 'PASSWORD_HERE';" guacamole_db
+psql -c "CREATE USER guacamole_user WITH PASSWORD '{{ postgres_password }}';" guacamole_db
 psql -c "GRANT SELECT,INSERT,UPDATE,DELETE ON ALL TABLES IN SCHEMA public TO guacamole_user;" guacamole_db
 psql -c "GRANT SELECT,USAGE ON ALL SEQUENCES IN SCHEMA public TO guacamole_user;" guacamole_db
