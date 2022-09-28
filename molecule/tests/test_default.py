d390ff506e2c0fb61353b052cad7dff0f27afdec
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-09-28 16:21:54 -0400

    Install the latest version of the Guacamole Docker composition
    See cisagov/guacamole-composition#52 for more details.

--- molecule/tests/test_default.py
+++ molecule/tests/test_default.py
@@ -71,7 +71,7 @@ def test_apache2_unit_modification(host):
 @pytest.mark.parametrize(
     "image",
     [
-        "cisagov/guacscanner:1.1.13",
+        "cisagov/guacscanner:1.1.15",
         "guacamole/guacd:1.4.0",
         "guacamole/guacamole:1.4.0",
         "postgres:13",
